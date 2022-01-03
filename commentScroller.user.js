929e5426f9474d784348fd7ce5ea210a450cd063
nn-nito <retou.1007@gmail.com>
2022-01-03 09:10:12 +0900

    リファクタリング

--- commentScroller.user.js
+++ commentScroller.user.js
@@ -7,8 +7,6 @@
 // @grant       none
 // @noframes
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
-// @require https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js
-// @require https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineMax.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/velocity/1.5.0/velocity.min.js
 // ==/UserScript==
 
@@ -20,20 +18,19 @@
 	 * 行に並べられる最大数
 	 * ※ここをいじるとコメントの大きさを変えることができます
 	 */
-	const MAX_LINE_COUNT = 13;
+	const MAX_LINE_COUNT = 14;
 
 	/**
-	 * コメントの生存時間
+	 * コメントの生存時間(ミリ秒)
 	 * ※ここをいじるとコメントの流れる速さを変えることができます
-	 * 例）8000ミリ秒=8秒
 	 */
-	const DURATION = 6000;
+	const DURATION = 5000;
 
 	/**
 	 * コメントの不透明度
 	 * 0.0（完全に透明）～1.0（完全に不透明)
 	 */
-	const OPACITY = 0.40;
+	const OPACITY = 0.50;
 
 	/**
 	 * コメントの色
@@ -46,6 +43,7 @@
 	const dataKey = 'nbacomment';
 	let retry = 1000, $screen;
 
+	/** コメント処理 */
 	$.fn.comment = function (message) {
 		if (message == null || message == '') {
 			return;
@@ -94,129 +92,79 @@
 		});
 		comment.show();
 
-		var completeHandler = function (thd, ind) {
-			comment.remove();
-			if (typeof thd === 'function') return;
-			if (data1.rows[ind] == comment) {
-				data1.rows[ind] = null;
-				own.data(dataKey, data1);
-			}
-		};
+		/** アニメーションライブラリ 色々試したけどパフォーマンスが一番いいのはVelocityぽい */
 
-		// cssアニメーション
+		// TweenMax CSS
+		// https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js
 		// TweenMax.to(comment[0], time / 1000, {
 		// 	css: { left: (-comment.width()) + 'px' },
-		// 	// css: { left: (-comment.width() - 10) + 'px' },
-		// 	onComplete: completeHandler,
-		// 	onCompleteParams: [this.data, index],
+		// 	onComplete: () => comment.remove(),
 		// 	ease: Linear.easeNone,
 		// });
 
-		// trasrate
+		// TweenMax Translate
+		// https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js
 		// TweenMax.to(comment[0], time / 1000, {
 		// 	x: -(this.width() + comment.width()),
-		// 	// css: { left: (-comment.width() - 10) + 'px' },
-		// 	onComplete: completeHandler,
-		// 	onCompleteParams: [this.data, index],
+		// 	onComplete: () => comment.remove(),
 		// 	ease: Linear.easeNone,
 		// });
 
-		// timeline
-		// var timeline = new TimelineMax({onComplete:completeHandler, onCompleteParams: [this.data, index],});
+		// Timeline
+		// https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineMax.min.js
+		// var timeline = new TimelineMax({onComplete: () => comment.remove()});
 		// timeline.to(comment[0], time / 1000, { x: -(this.width() + comment.width()), ease: Linear.easeNone,});
-		// 良さそう
-		// timeline.to(comment[0], time / 1000, { x: -(this.width() + comment.width()), ease: Strong.easeIn,});
 
-
-		// velocity
+		// Velocity
+		// https://cdnjs.cloudflare.com/ajax/libs/velocity/1.5.0/velocity.min.js
 		comment.velocity(
 			{
 				translateX: -(this.width() + comment.width()) + 'px',
 			},
 			{
-				// Option
-				duration: time, // アニメーション時間
-				easing: 'linear', // イージング : linear, swing, ease, ease-in, ease-out, ease-in-out, [200, 15]
-				complete: function (elements) {
-					comment.remove();
-				},
+				duration: time,
+				easing: 'linear',
+				complete: () => comment.remove(),
 			}
 		);
+		/** */
 
 		// data属性設定
 		this.data(dataKey, data);
 	}
 
+	/** メイン処理 */
 	function main() {
-		//=======================================
-		// テスト用
-		let $screen = $('.pGamesDetail-video-container.video-fullscreen');
-		// let b = document.querySelector('#mainContainer');
-		let $a = $('#mainContainer');
-		if (!$screen.length || !$a.length || retry > 800) {
-			window.setTimeout(function () {
-				if (retry--) main();
-			}, 10);
-			return;
-		}
+		document.body.addEventListener('DOMNodeInserted', function (e) {
+			if (e.target.className == 'pGamesDetail-video-chat slide-enter slide-enter-active') {
+				// 初回読み込みで多量にコメントを検知してしまうので初回はsetTimeout使用
+				window.setTimeout(function () {
+					$screen = $('#mainContainer');
+					$screen.css({
+						'white-space': 'nowrap',
+						'overflow': 'hidden',
+						'position': 'relative',
+					});
 
-		// screen.style.whiteSpace = 'nowrap';
-		// screen.style.overflow = 'hidden';
-		// screen.style.position = 'relative';
-		$screen.css({
-			'white-space': 'nowrap',
-			'overflow': 'hidden',
-			'position': 'relative',
+					updateStyle($screen.height() / MAX_LINE_COUNT);
+
+					var chatContainer = document.querySelector('.ChatList-container');
+					chatContainer.addEventListener('DOMNodeInserted', function (e) {
+						let comment = e.target;
+						if (comment.className != 'ChatCard') return;
+						let c = comment.querySelector('.bTypography__variant_body2');
+						$screen.comment(c.textContent);
+					});
+				}, 800);
+			}
 		});
-
-		// var $a = $('#mainContainer');
-		updateStyle($a.height() / MAX_LINE_COUNT);
-		// console.log($a.height() / MAX_LINE_COUNT)
-
-		var i = 0;
-		var interval = window.setInterval(function () {
-			// console.log($a.height());
-			i++;
-			$a.comment('ああああかきくけこさしす' + i);
-		}, 1000 / 3);
 
 		$(window).resize(function () {
+			if ($screen.length === 0) return;
+			// リサイズしたらコメント全て削除
 			$('.nba-comment').remove();
-			updateStyle($a.height() / MAX_LINE_COUNT);
+			updateStyle($screen.height() / MAX_LINE_COUNT);
 		});
-		//=======================================
-
-
-
-		// document.body.addEventListener('DOMNodeInserted', function (e) {
-		// 	if (e.target.className == 'pGamesDetail-video-chat slide-enter slide-enter-active') {
-		// 		window.setTimeout(function () {
-		// 			$screen = $('#mainContainer');
-		// 			$screen.css({
-		// 				'white-space': 'nowrap',
-		// 				'overflow': 'hidden',
-		// 				'position': 'relative',
-		// 			});
-
-		// 			updateStyle($screen.height() / MAX_LINE_COUNT);
-
-		// 			var chatContainer = document.querySelector('.ChatList-container');
-		// 			chatContainer.addEventListener('DOMNodeInserted', function (e) {
-		// 				let comment = e.target;
-		// 				if (comment.className != 'ChatCard') return;
-		// 				let c = comment.querySelector('.bTypography__variant_body2');
-		// 				$screen.comment(c.textContent);
-		// 			});
-		// 		}, 1000);
-		// 	}
-		// });
-
-		// $(window).resize(function () {
-		// 	if ($screen.length === 0) return;
-		// 	// リサイズしたらコメント全て削除
-		// 	$('.nba-comment').remove();
-		// 	updateStyle($screen.height() / MAX_LINE_COUNT);
-		// });
 	}
 
 	/** スタイルを更新 */
